beb3e1e1c40b20af0425ad6adfcdb834feac3251
Tinu Thomas <tj20thom@siena.edu>
2023-01-07 23:07:27 -0500

    Migrated html + css into typescript using Next.JS. Will be deploying using Vercel momentarily. Deleted temp + old html & css files

--- homepage-project/pages/index.tsx
+++ homepage-project/pages/index.tsx
@@ -1,11 +1,40 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "../styles/Home.module.css";
+import bg from "./public/background-image.jpg";
+import Link from "next/link";
+import Script from "next/script";
+import { useCallback, useEffect, useState, useRef } from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const homeRef = useRef();
+  const aboutRef = useRef();
+  const portfolioRef = useRef();
+  const contactRef = useRef();
+  const getYear = new Date().getFullYear();
+  const [scrolled, setScrolled] = useState(false);
+
+  const handleScroll = () => {
+    const offset = window.scrollY;
+
+    if (offset > window.innerHeight - 100) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+  }, []);
+
+  function handleBackClick(ref: any) {
+    ref?.current?.scrollIntoView({ behavior: "smooth" });
+  }
+
   return (
     <>
       <Head>
@@ -14,14 +43,129 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" /> */}
         <title>Personal Website</title>
-        <meta name="description" content="Personal Website Created By Tinu Thomas" />
+        <meta name="description" content="Personal Website Created By" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
-        <link href='https://fonts.cdnfonts.com/css/proxima-nova-2' rel='stylesheet' type='text/css' />
+        {/* <img src = "/background-image.jpg" alt = "background-image" className = {styles.backgroundImage} /> */}
+        <link
+          href="https://fonts.cdnfonts.com/css/proxima-nova-2"
+          rel="stylesheet"
+          type="text/css"
+        />
       </Head>
-      <main className={styles.main}>
-        
+      {/* @ts-ignore */}
+      <main className={styles.main} ref={homeRef}>
+        <div className={styles.combine}>
+          <nav className={scrolled ? styles.scrolled : styles.navbar}>
+            <ul>
+              <li>
+                <a onClick={() => handleBackClick(homeRef)}>Home</a>
+              </li>
+              <li>
+                <a onClick={() => handleBackClick(aboutRef)}>About</a>
+              </li>
+              <li>
+                <a onClick={() => handleBackClick(portfolioRef)}>Portfolio</a>
+              </li>
+              <li>
+                <a onClick={() => handleBackClick(contactRef)}>Contact Me</a>
+              </li>
+            </ul>
+          </nav>
+          <div id={styles.headerDiv}>
+            <section id={styles.header}>
+              <div className={styles.bio}>
+                <h1>
+                  Tinu <div className={styles.last}>Thomas</div>
+                </h1>
+                <h3>Web Developer</h3>
+                <h3>Student & Siena College</h3>
+              </div>
+            </section>
+          </div>
+        </div>
+        {/* about section */}
+        {/* @ts-ignore */}
+        <section id={styles.aboutSection} ref={aboutRef}>
+          <div className={styles.mainAbout}>
+            <img src="/about-photo.jpg" alt="About Photo" />
+            <div className={styles.aboutMe}>
+              <h2>
+                About <div className={styles.last}>Me</div>
+              </h2>
+              <p>
+                Aspiring Computer Science student at Siena College, with minor
+                in Data Science. I am interested in front-end web development as
+                well as database management and am seeking opportunities related
+                to these. Having worked in a fast-paced environment in the past,
+                I am able to adapt to changes quickly while also maintaining
+                both organizational and leadership skills. I've been using these
+                skills at New York State's Office of Information Technology
+                Services. I've been working as an intern within the Department
+                of Transportation sector. I'm looking to showcase the skills
+                I've learned while working as an intern and expanding these
+                skills.
+              </p>
+            </div>
+          </div>
+        </section>
+        {/* portfolio section */}
+        {/* @ts-ignore */}
+        <section id={styles.portfolioSection} ref={portfolioRef}>
+          <h1 className={styles.portfolioHeading}>Portfolio</h1>
+          <div className={styles.gallery}>
+            <img src="/portfolio1.jpg" alt="Portfolio 1" />
+          </div>
+        </section>
+        {/* contact section */}
+        {/* @ts-ignore */}
+        <section id={styles.contact} ref={contactRef}>
+          <h1 className={styles.contactHeading}>
+            Contact <div className={styles.last}>Me</div>
+          </h1>
+          <form action="/index.tsx" method="POST" className={styles.form}>
+            <input
+              type="text"
+              name="name"
+              className={styles.formBox}
+              placeholder="Name"
+            ></input>
+            <input
+              type="email"
+              name="email"
+              className={styles.formBox}
+              placeholder="Email"
+            ></input>
+            <input
+              type="text"
+              name="subject"
+              className={styles.formBox}
+              placeholder="Subject"
+            ></input>
+            <textarea
+              name="message"
+              id={styles.messageBox}
+              placeholder="Message"
+            ></textarea>
+            <input type="submit" value="Send" id={styles.sendButton}></input>
+          </form>
+        </section>
+        {/* footer section */}
+        <footer className={styles.footer}>
+          <div className={styles.icons}>
+            <a href="https://github.com/tjt49">
+              <img src="./github.png" alt="Github" />
+            </a>
+            <a href="https://www.instagram.com/tthomas49/">
+              <img src="./instagram.png" alt="Instagram" />
+            </a>
+            <a href="https://www.linkedin.com/in/tinu-thomas/">
+              <img src="./linkedin.png" alt="LinkedIn" />
+            </a>
+          </div>
+          <p>Copyright &copy;{getYear} Tinu Thomas</p>
+        </footer>
       </main>
     </>
-  )
+  );
 }
